fab608e228cb62b643228b581e8a8b9a8826df9f
RadonZ <supachai27102000@gmail.com>
2024-06-14 14:41:47 +0700

    update: home page

--- clients/app/page.tsx
+++ clients/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
+import Navigation from "./components/Navigation";
 
-export default function Home() {
+export default function Page() {
   return (
-    <h1>Hello Next!</h1>
+    <div>
+    <Navigation />
+    <h1>The Wild Oasis. Welcome to paradise</h1>
+
+    <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+
   );
 }
