f5a03a8613a815f1dfed7bcbb60d8155d45e8eb6
RadonZ <supachai27102000@gmail.com>
2024-06-19 13:42:07 +0700

    remove: unused navigation part

--- clients/app/cabins/page.tsx
+++ clients/app/cabins/page.tsx
@@ -1,11 +1,23 @@
-import Navigation from "../components/Navigation"
+import Counter from "../components/Counter";
 
 
-export default function Page() {
+
+
+export default async function Page() {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+  const data = await res.json();
+
+  
+
   return (
     <div>
-      <Navigation/>
     <h1>Cabins page</h1>
+    <ul>{data.map((user: {id: number, name: string}) => <li key={user.id}>
+      <li>{user.name}</li>
+    </li>)}</ul>
+    <Counter/>
     </div>
   )
 }
