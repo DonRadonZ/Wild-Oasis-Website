51f5116ec14b5ae77bed162dbe13fb1fb6c055fd
RadonZ <supachai27102000@gmail.com>
2024-06-21 15:07:14 +0700

    refactor: layout design

--- clients/app/layout.tsx
+++ clients/app/layout.tsx
@@ -30,12 +30,12 @@ export const metadata = {
 export default function RootLayout({children}: RootLayoutProps) {
   return (
   <html lang="en">
-    <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+    <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
       
       <Header/>
 
-    <div className="flex-1 px-8 py-12">
-      <main className="max-w-7xl mx-auto">
+    <div className="flex-1 px-8 py-12 grid">
+      <main className="max-w-7xl mx-auto w-full">
         {children}
       </main> 
       </div>  
