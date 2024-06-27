bb894c2f6e8706b6964b7d941bd2b821cc8d82e7
RadonZ <supachai27102000@gmail.com>
2024-06-27 17:50:48 +0700

    add update Reservation action

--- clients/app/_lib/actions.ts
+++ clients/app/_lib/actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import {auth ,signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
     const session = await auth();
@@ -25,14 +26,56 @@ export async function updateGuest(formData) {
     if (error) throw new Error("Guest could not be updated");
   
     revalidatePath("/account/profile");
+}
+
+export async function updateReservation(formData) {
+  const bookingId = formData.get("bookingId");
+
+  // 1) Authentication
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  // 1) Authorization
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  // Logging for debugging
+  console.log("Session User Guest ID:", session.user.guestId);
+  console.log("Booking ID from formData:", bookingId);
+  console.log("Guest Booking IDs:", guestBookingIds);
+
+  if (!guestBookingIds.includes(bookingId)) throw new Error("You are not allowed to update this booking");
+
+  // 3) Building update data
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000)
+  };
+
+  // 4) Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // 5) Error handling
+  if (error) {
+    console.error(error);
+    throw new Error('Booking could not be updated');
   }
 
-export async  function deleteReservation(bookingId) {
-    const session = await auth();
-    if (!session) throw new Error("You must be logged in");    
-    
-    
+  revalidatePath("/account/reservations");
 
+  // 7) Redirecting
+  redirect("/account/reservations");
+}
+
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+    
  const guestBookings = await getBookings(session.user.guestId);
 
  const guestBookingIds = guestBookings.map((booking) => booking.id);
