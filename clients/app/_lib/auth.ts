0b5a27598a9432268f8d75d5aabeb429acb194db
RadonZ <supachai27102000@gmail.com>
2024-06-27 13:56:30 +0700

    fix: type auth

--- clients/app/_lib/auth.ts
+++ clients/app/_lib/auth.ts
@@ -1,11 +1,18 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth, { 
+  Account, 
+  DefaultSession, 
+  Profile, 
+  Session, 
+  User 
+} from "next-auth";
 import Google from "next-auth/providers/google";
 import { createGuest, getGuest } from "./data-service";
+import { NextRequest } from "next/server";
 
 declare module "next-auth" {
     interface Session {
-        user: {
-            guestId: number | undefined;
+        user: User & {
+            guestId: number;
         } & DefaultSession["user"];
     }
 }
@@ -16,26 +23,54 @@ const authConfig = {
         clientId: process.env.AUTH_GOOGLE_ID,
         clientSecret: process.env.AUTH_GOOGLE_SECRET,
       }),
-    ],
+    ],   
     callbacks: {
-        authorized({ auth, request }) {
+        authorized({
+          auth,
+          request
+         }: {
+          auth: Session | null;
+          request: NextRequest;
+         }) {
           return !!auth?.user;
         },
-        async signIn({ user, account, profile }) {
+        async signIn({
+           user, 
+           account, 
+           profile 
+          }:{
+            user: User;
+            account: Account | null;
+            profile?: Profile | undefined;
+          }) {
           try {
             const existingGuest = await getGuest(user.email);
+            console.log({existingGuest});
     
-            if (!existingGuest)
-              await createGuest({ email: user.email, fullName: user.name });
-    
+            if (!existingGuest){
+              if(!user.email || !user.name)
+                throw new Error("Missing necessary fields");
+
+              await createGuest({ 
+                email: user.email, 
+                fullName: user.name,
+              });
+            }
             return true;
-          } catch {
+          } catch(error) {
+            console.error(error);
             return false;
           }
         },
-        async session({ session, user }) {
-          const guest = await getGuest(session.user.email);
+        async session({ session, user }: {session: Session; user: User }) {
+          const guest = await getGuest(session.user?.email);
+
+          if (!guest || isNaN(guest?.id) || !session.user){
+            throw new Error("Something went wrong");
+          }
+
           session.user.guestId = guest.id;
+
           return session;
         },
       },
@@ -49,4 +84,4 @@ export const {
     signIn,
     signOut, 
     handlers: { GET, POST } 
-} = NextAuth(authConfig)
+} = NextAuth(authConfig);
